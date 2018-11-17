84a48bdd398d8e948610680a49c1be8f79efb24d
Angenis Garcia <angenis_garcias17@hotmail.com>
2018-11-17 16:27:14 -0400

    API? CON QUE SE COME ESO

--- php_rest_api/models/Contratodocencia.php
+++ php_rest_api/models/Contratodocencia.php
@@ -1,18 +1,18 @@
 <?php
-class Grupoinsest{
+class contratodocencia{
     // DB stuff
     private $conn;
-    private $table='grupoinsest';
+    private $table='contratodocencia';
 
     //Estudiante Properties
-    public $Matricula;
     public $CodTema;
-    public $CodTP; 
-    public $Numgrupo;
+    public $CodTp;
+    public $Numgrupo; 
     public $CodCampus;
     public $AnoAcad;
     public $NumPer;
-    public $NumAusencias;
+    public $NumCedula;
+    
     
     //Constructor with BD
     public function __construct($db) {
@@ -21,7 +21,7 @@ class Grupoinsest{
     // Get grupo activo
     public function read(){
         //Create query
-        $query='SELECT Matricula,CodTema,CodTP,Numgrupo,CodCampus,AnoAcad,NumPer,NumAusencias FROM '.$this->table.' ';
+        $query='SELECT CodTema,CodTp,Numgrupo,CodCampus,AnoAcad,NumPer,NumCedula FROM '.$this->table.' ';
         // Prepare statemnt
         $stmt = $this->conn->prepare($query);
         //Execute query
@@ -33,36 +33,35 @@ class Grupoinsest{
         // Create queary
         $query = 'INSERT INTO '.$this->table.'
             SET
-                Matricula = :Matricula,
                 CodTema = :CodTema,
-                CodTP= :CodTP,
+                CodTp = :CodTp,
                 Numgrupo= :Numgrupo,
                 CodCampus= :CodCampus,
                 AnoAcad= :AnoAcad,
                 NumPer= :NumPer,
-                NumAusencias=:NumAusencias';
+                NumCedula= :NumCedula';
 
         //Prepare statement
         $stmt = $this->conn->prepare($query);
         //Clean data
-        $this->Matricula = htmlspecialchars(strip_tags($this->Matricula));
         $this->CodTema = htmlspecialchars(strip_tags($this->CodTema));
-        $this->CodTP = htmlspecialchars(strip_tags($this->CodTP));
+        $this->CodTp = htmlspecialchars(strip_tags($this->CodTp));
         $this->Numgrupo = htmlspecialchars(strip_tags($this->Numgrupo));
         $this->CodCampus = htmlspecialchars(strip_tags($this->CodCampus));
         $this->AnoAcad = htmlspecialchars(strip_tags($this->AnoAcad));
         $this->NumPer = htmlspecialchars(strip_tags($this->NumPer));
-        $this->NumAusencias = htmlspecialchars(strip_tags($this->NumAusencias));
+        $this->NumCedula = htmlspecialchars(strip_tags($this->NumCedula));
+        
 
         //Bind data
-        $stmt->bindParam(':Matricula',$this->Matricula);
         $stmt->bindParam(':CodTema',$this->CodTema);
-        $stmt->bindParam(':CodTP',$this->CodTP);
+        $stmt->bindParam(':CodTp',$this->CodTp);
         $stmt->bindParam(':Numgrupo',$this->Numgrupo);
         $stmt->bindParam(':CodCampus',$this->CodCampus);
         $stmt->bindParam(':AnoAcad',$this->AnoAcad);
         $stmt->bindParam(':NumPer',$this->NumPer);
-        $stmt->bindParam(':NumAusencias',$this->NumAusencias);
+        $stmt->bindParam(':NumCedula',$this->NumCedula);
+        
         
         //Execute query
         if($stmt->execute()){
@@ -73,8 +72,6 @@ class Grupoinsest{
         return false;
     }
 
-
-
-
 }
+
 ?>
